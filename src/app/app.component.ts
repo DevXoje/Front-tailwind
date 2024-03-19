02ff1d00ac25edc6eba3eadc61ee1913a1699e5a
Jose Vilches <jose@crmble.com>
2024-03-19 08:26:37 +0100

    Add Tailwind CSS and PostCSS dependencies, import header and footer components, update app routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { HeaderComponent } from './pages/partials/header/header.component';
+import { FooterComponent } from './pages/partials/footer/footer.component';
+const components = [HeaderComponent, FooterComponent];
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ...components],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { SearcherComponent } from './pages/product/searcher/searcher.component';
+import { LoginComponent } from './pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'cart',
+        component: CartComponent
+    },
+    {
+        path: 'catalog',
+        component: SearcherComponent
+    },
+    {
+        path: '**', component: NotFoundComponent
+    },
+
+];
